c2ded8d94df125c85d95ca7fce75c79982d5077d
Tanishka666 <tanishkagandhi2805@gmail.com>
2026-06-17 17:45:27 +0530

    Update predictor.py

--- Backend/predictor.py
+++ Backend/predictor.py
@@ -1,1 +1,12 @@
+from model_loader import load_model
 
+model = load_model()
+
+def predict_health(data):
+    features = [data.get('heart_rate'), data.get('steps'), data.get('glucose')]
+    prediction = model.predict([features])[0]
+    return {
+        "user_id": data.get("user_id"),
+        "prediction": prediction,
+        "features": features
+    }
