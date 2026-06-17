567a89d06621f37ebe09a598cff86dd4d737c9b6
Tanishka666 <tanishkagandhi2805@gmail.com>
2026-06-17 17:42:51 +0530

    Update db.py

--- Backend/db.py
+++ Backend/db.py
@@ -1,1 +1,7 @@
+from pymongo import MongoClient
 
+client = MongoClient("mongodb://localhost:27017/")
+db = client["healthtracker"]
+
+users = db["users"]
+healthdata = db["healthdata"]  # <-- ADD THIS
